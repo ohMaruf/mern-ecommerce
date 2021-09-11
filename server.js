74004765ac4ecdeb9fb6b4c7b805dc73b7913184
Ahmed <marufishtiaqueahmed@gmail.com>
2021-09-11 17:49:05 +0200

    port change

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ if (process.env.NODE_ENV === "production") {
   })
 }
 
-
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log("Server avviato alla porta " + port);
